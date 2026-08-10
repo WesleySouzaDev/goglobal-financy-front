3ba1ecf28dce71028aeee82463c4abefb048f13c
WesleySouzaDev <wesleydesouza501@gmail.com>
2026-08-10 01:47:59 -0300

    refactor: improvents for libs

--- components/password-input.tsx
+++ components/password-input.tsx
@@ -1,53 +1,40 @@
 "use client";
 
-import { FaLock } from "react-icons/fa";
-import { FaLockOpen } from "react-icons/fa";
-
-import { useState } from "react";
-
+import { FaLock, FaLockOpen } from "react-icons/fa";
+import { forwardRef, useState } from "react";
 import { cn } from "@/lib/utils";
 
-type PasswordInputProps = {
-	className?: string;
-	placeholder?: string;
-	id?: string;
-	name?: string;
-};
+type PasswordInputProps = React.ComponentProps<"input">;
 
-export function PasswordInput({
-	className,
-	placeholder = "Escreva sua senha...",
-	id,
-	name,
-	...props
-}: PasswordInputProps) {
-	const [password, setPassword] = useState(false);
+export const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
+	({ className, placeholder = "Escreva sua senha...", ...props }, ref) => {
+		const [password, setPassword] = useState(true);
 
-	return (
-		<div className="relative">
-			<input
-				type={password ? "password" : "text"}
-				placeholder={placeholder}
-				id={id}
-				data-slot="input"
-				className={cn(
-					"h-9 w-full min-w-0 rounded-md border border-input bg-transparent pl-2.5 pr-11 py-1 truncate text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
-					className,
+		return (
+			<div className="relative">
+				<input
+					{...props}
+					ref={ref}
+					type={password ? "password" : "text"}
+					placeholder={placeholder}
+					className={cn(
+						"h-9 w-full min-w-0 rounded-md border border-input bg-transparent pl-2.5 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 pr-11",
+						className,
+					)}
+				/>
+
+				{password ? (
+					<FaLock
+						className="absolute top-2.5 right-4 cursor-pointer text-foreground/70"
+						onClick={() => setPassword(false)}
+					/>
+				) : (
+					<FaLockOpen
+						className="absolute top-2.5 right-4 cursor-pointer text-foreground/70"
+						onClick={() => setPassword(true)}
+					/>
 				)}
-				{...props}
-			/>
-
-			{password ? (
-				<FaLock
-					className="absolute top-2.5 right-4 text-foreground/50 cursor-pointer bg-background"
-					onClick={() => setPassword(!password)}
-				/>
-			) : (
-				<FaLockOpen
-					className="absolute top-2.5 right-4 text-foreground/50 cursor-pointer bg-background"
-					onClick={() => setPassword(!password)}
-				/>
-			)}
-		</div>
-	);
-}
+			</div>
+		);
+	},
+);
