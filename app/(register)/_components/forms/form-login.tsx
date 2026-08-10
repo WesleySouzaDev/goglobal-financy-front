1fc5fa2c1d5866adcfc260d0f063d0b98b41bd13
WesleySouzaDev <wesleydesouza501@gmail.com>
2026-08-10 01:45:59 -0300

    feat: create login form validate

---
+++ app/(register)/_components/forms/form-login.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Field, FieldLabel, FieldError } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+import { PasswordInput } from "@/components/password-input";
+
+import { Button } from "@/components/ui/button";
+import { FaDoorOpen } from "react-icons/fa6";
+
+const schema = z.object({
+	email: z.email("E-mail inválido"),
+	password: z
+		.string()
+		.min(6, "A senha deve conter no mínimo 6 caracteres")
+		.regex(/[a-z]/, "A senha deve conter no mínimo uma letra")
+		.regex(/[0-9]/, "A senha deve conter um no mínimo número"),
+});
+
+type FormLoginData = z.infer<typeof schema>;
+
+export default function FormLogin() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm<FormLoginData>({
+		resolver: zodResolver(schema),
+	});
+
+	const onSubmit = (data: FormLoginData) => console.log(JSON.stringify(data));
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<div className="space-y-6">
+				<Field>
+					<FieldLabel htmlFor="email" className="text">
+						E-mail
+					</FieldLabel>
+					<Input
+						id="email"
+						placeholder="Escreva seu e-mail..."
+						className="w-full min-w-sm"
+						{...register("email")}
+					/>
+					{errors.email && <FieldError errors={[errors.email]}></FieldError>}
+				</Field>
+
+				<Field>
+					<FieldLabel htmlFor="senha" className="text">
+						Senha
+					</FieldLabel>
+					<PasswordInput id="senha" {...register("password")} />
+					{errors.password && (
+						<FieldError errors={[errors.password]}></FieldError>
+					)}
+				</Field>
+
+				<Button type="submit" className="bg-primary/50 cursor-pointer">
+					Entrar
+					<FaDoorOpen />
+				</Button>
+			</div>
+		</form>
+	);
+}
