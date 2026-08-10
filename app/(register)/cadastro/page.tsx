6ce862d6ba5e89c39b44f9de6b7cfd0c1f73e70c
WesleySouzaDev <wesleydesouza501@gmail.com>
2026-08-09 23:35:56 -0300

    feat: add register page UI

---
+++ app/(register)/cadastro/page.tsx
@@ -0,0 +1,66 @@
+import Title from "@/components/title";
+
+import { Button } from "@/components/ui/button";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+
+import Link from "next/link";
+
+export default async function Cadastro() {
+	return (
+		<div className="w-full h-full flex flex-col items-start animeRight">
+			<Title titile="Cadastre-se" className="text-primary title text-end" />
+
+			<div className="mt-15 mb-6">
+				<form action="">
+					<div className="space-y-6">
+						<Field>
+							<FieldLabel htmlFor="nome" className="text">
+								Nome
+							</FieldLabel>
+							<Input
+								id="nome"
+								placeholder="Escreva seu nome..."
+								className="w-full min-w-sm"
+							/>
+							{true && <FieldError errors={[]}>error</FieldError>}
+						</Field>
+						<Field>
+							<FieldLabel htmlFor="email" className="text">
+								E-mail
+							</FieldLabel>
+							<Input
+								id="email"
+								placeholder="Escreva seu e-mail..."
+								className="w-full min-w-sm"
+							/>
+							{true && <FieldError errors={[]}>error</FieldError>}
+						</Field>
+
+						<Field>
+							<FieldLabel htmlFor="senha" className="text">
+								Senha
+							</FieldLabel>
+							<Input
+								id="senha"
+								placeholder="Escreva sua senha..."
+								className="w-full min-w-sm"
+							/>
+							{true && <FieldError errors={[]}>error</FieldError>}
+						</Field>
+					</div>
+				</form>
+			</div>
+
+			<Separator />
+
+			<h2 className="text-3xl text-foreground mt-10">
+				Faça Login se já possui uma conta
+			</h2>
+			<Button variant="link" className="p-0 my-2">
+				<Link href="/login">← Clique para fazer login</Link>
+			</Button>
+		</div>
+	);
+}
---
+++ app/(register)/layout.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Globla Financy | Login",
+	description: "Register Page",
+};
+
+export default async function RegisterLayout({ children }: any) {
+	return (
+		<main className="grid grid-cols-1 lg:grid-cols-2 h-screen w-screen overflow-hidden bg-background">
+			<div className="hidden lg:block lg:grid-cols-1 w-full h-full border-r border-primary animeSurge">
+				<Image
+					src="/registerImg2.jpg"
+					alt="Register Image"
+					width={1000}
+					height={1000}
+					loading="lazy"
+					className="object-cover h-full brightness-50"
+				/>
+			</div>
+
+			<div className="grid-cols-1 px-20 py-20 bg-background">{children}</div>
+		</main>
+	);
+}
